dfea24cb8badf6ea26efd44110b491b3818b185e
JulioneQA <neroazurro@gmail.com>
2025-01-09 19:24:48 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = https://cnt-bd04ca78-aa85-440b-b585-86035d025c91.containerhub.tripleten-services.com
-CREATE_USER_PATH = https://cnt-bd04ca78-aa85-440b-b585-86035d025c91.containerhub.tripleten-services.com/api/v1/users
-KITS_PATH = https://cnt-bd04ca78-aa85-440b-b585-86035d025c91.containerhub.tripleten-services.com/api/v1/users/api/v1/kits
+URL_SERVICE ="https://cnt-5769b7bd-8963-4dd8-82d6-01b96b8184e8.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users"
+KITS_PATH = "/api/v1/kits"
