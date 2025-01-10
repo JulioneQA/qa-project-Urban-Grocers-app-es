fd25ccc96bfd79bf451da41509e0c1e51cc970d0
JulioneQA <neroazurro@gmail.com>
2025-01-09 22:01:41 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 headers= {
-    "content_type":"application/json"
-    }
+        "content_type":"application/json"
+        }
 
 user_body={
-    "firstName": "Max",
-    "phone": "+10005553535"
-    "address": "8042 Lancaster Ave.Hamburg,NY"
-}
+        "firstName": "Max",
+        "phone": "+10005553535",
+        "address": "8042 Lancaster Ave.Hamburg,NY"
+    }
