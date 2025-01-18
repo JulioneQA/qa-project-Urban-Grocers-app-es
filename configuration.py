fc1ac1e8790495cbf70614734b012180f17ebc2e
JulioneQA <neroazurro@gmail.com>
2025-01-18 07:36:35 -0500

    Update README.md

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE ="https://cnt-f59d0da1-2d6b-4fac-b197-e6ccf66e329b.containerhub.tripleten-services.com"
+URL_SERVICE ="https://cnt-45534151-7191-40b8-8473-4eae4968ffda.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
