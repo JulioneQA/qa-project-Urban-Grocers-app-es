b29c592d63063a86803b530b30119e6e89d75268
JulioneQA <neroazurro@gmail.com>
2025-02-10 22:50:06 -0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,26 +2,13 @@ import configuration
 import requests
 import data
 
-def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH ,
-                     json= body,
-                     headers= data.headers    )
-
-    # Guardar el token de autenticación
-    return resp_user.json()["authToken"]
-
-auth_token = get_new_user_token()
-
-Headers2= {
-    "content_type":"application/json",
-    "Authorization": f'Bearer{auth_token}'
-    }
 
 def post_personal_kit(kitbody):
     return requests.post(configuration.URL_SERVICE+configuration.KITS_PATH,
                          json=kitbody,
-                         headers=Headers2)
+                         headers=data.headers)
 
-
-def post_new_client_kit(kit_body, name):
-    return None
+def post_new_client_kit(kit_body,auth_token):
+    return requests.post(configuration.URL_SERVICE+configuration.KITS_PATH,
+                         json=kitbody,
+                         headers=auth_token)
