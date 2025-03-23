3a7cdda96ad9b6fd47d2bf48ec7e61791a39c050
JulioneQA <neroazurro@gmail.com>
2025-03-23 10:19:49 -0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,12 +3,13 @@ import requests
 import data
 
 
-def post_personal_kit(kitbody):
-    return requests.post(configuration.URL_SERVICE+configuration.KITS_PATH,
-                         json=kitbody,
+def post_personal_kit(body):
+    return requests.post(configuration.URL_SERVICE+configuration.CREATE_USER_PATH,
+                         json=body,
                          headers=data.headers)
 
 def post_new_client_kit(kit_body,auth_token):
     return requests.post(configuration.URL_SERVICE+configuration.KITS_PATH,
-                         json=kitbody,
+                         json=kit_body,
                          headers=auth_token)
+
